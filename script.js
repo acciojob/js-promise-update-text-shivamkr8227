bfc5de9a61787397d650d1659773fe77960fe389
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-01 21:19:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function sayhello(){
+	return new promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello world");
+		})
+	});
+}
+const outpt=document.getElementById("output");
+sayhello().then((message)=>{
+	outpt.textContent=message;
+});
