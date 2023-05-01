25c88f7cf498da8773f5b13f3cb381fa01cad696
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-01 23:09:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 //your JS code here. If required.
 
-function sayhello(comp){
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			if(comp)
-			{
-				resolve("Hello, world!");
-			}
-			else{
-				reject("Error");
-			}
-		},1000);
-	});
+// function sayhello(comp){
+// 	return new Promise((resolve,reject)=>{
+// 		setTimeout(()=>{
+// 			if(comp)
+// 			{
+// 				resolve("Hello, world!");
+// 			}
+// 			else{
+// 				reject("Error");
+// 			}
+// 		},1000);
+// 	});
+// }
+// const outpt=document.getElementById("output");
+// let protrue=(messsage)=>{
+// 	outpt.textContent=message;
+// }
+// let profalse=(error)=>{
+// 	console.log(error);
+// }
+// sayhello(true).then(protrue).catch(profalse);
+
+const outputElement = document.getElementById("output");
+
+function delayedMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
 }
-const outpt=document.getElementById("output");
-let protrue=(messsage)=>{
-	outpt.textContent=message;
-}
-let profalse=(error)=>{
-	console.log(error);
-}
-sayhello(true).then(protrue).catch(profalse);
+
+delayedMessage().then((message) => {
+  outputElement.textContent = message;
+});
